1e508f7a3603ed0d2333007b9162d307c53f7f5d
Sofia <145608120+djooseo@users.noreply.github.com>
2023-10-12 16:47:13 +0300

    Add files via upload

---
+++ HW8.py
@@ -0,0 +1,57 @@
+"""
+1. Напишіть декоратор, який вимірює час виконання функції
+2. Задекоруйте цим декоратором вашу програму "Касир"
+"""
+import time
+
+
+def age_input(prompt):
+    try:
+        res = int(input(f'{prompt}'))
+        return res
+    except Exception:
+        print("Будь ласка введіть ваш реальний вік!")
+
+
+def word_form(res):
+    last_digit = res % 10
+    age = 'рік' if last_digit == 1 and res != 11 else 'роки' if 1 < last_digit < 5 and (
+            res < 10 or res > 20) else 'років'
+    return age
+
+
+def cashier_in_cinema(res):
+    if '7' in str(res):
+        print(f"Вам {res} {word_form(res)}, вам пощастить!")
+    elif res < 7:
+        print(f"Тобі ж {res} {word_form(res)}! Де твої батьки?")
+    elif res < 16:
+        print(f"Тобі лише {res} {word_form(res)}, а це фільм для дорослих!")
+    elif res > 65:
+        print(f"Вам {res} {word_form(res)}? Покажіть пенсійне посвідчення!")
+    else:
+        print(f"Незважаючи на те, що вам {res} {word_form(res)}, білетів всеодно нема!")
+    return
+
+
+def decorator_time(func):
+
+    def time_function(*args, **kwargs):
+        time_before_func = time.time()
+        res = func(*args, **kwargs)
+        time_after_func = time.time()
+        timer = time_after_func - time_before_func
+
+        return timer
+    return time_function
+
+
+@decorator_time
+def final_res():
+    prompt = 'Вкажіть ваш вік: '
+    res = age_input(prompt)
+    cashier_in_cinema(res)
+
+
+final_time_res = final_res()
+print(f'Пройшло {final_time_res} секунд.')
